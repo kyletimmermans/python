4947672e914609c6edca8e557c1742646d1b42c3
Kyle Timmermans <47165102+kyletimmermans@users.noreply.github.com>
2020-05-09 20:25:48 -0400

    Add files via upload

--- SearchPattern.py
+++ SearchPattern.py
@@ -5,13 +5,14 @@ Search for specific words in a document
 Inspired by question on Algorithms Final
 '''
 
+
 def SearchPattern(input, word):
     found = []
     n = len(input)
     m = len(word)
     for i in range(0, n):
         if input[i] == word[0]:  # Start checking the rest of the input, if the first letter matches the target word
-            for j in range(1, m):  # For the length of the target word
+            for j in range(1, m):  # For the length of the target word e.g. "o o d" in "good"
                 if input[i+j] == word[j]:  # Keep checking the next letter in the text to see if matches the target word
                     if input[i+j] == word[j] and j == m-1:   # Did it match the whole word?  -1 needed, first letter already checked (this for-loop started from 1)
                         found.append(i)  # return index of first letter of the word in the given string that matches target word
